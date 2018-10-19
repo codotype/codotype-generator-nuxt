94361f2e7679fcf4f2c8936ba9c4392103ce0229
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-19 18:44:40 -0400

    Updated meta.json && dependencies

--- generator/index.js
+++ generator/index.js
@@ -3,6 +3,9 @@ const Generator = require('@codotype/generator')
 module.exports = class NuxtJs extends Generator {
   async write () {
     await this.composeWith('./base')
+    await this.composeWith('./api_base')
+    await this.composeWith('./api_resource')
+    await this.composeWith('./api_environment')
     await this.composeWith('./auth')
     await this.composeWith('./navbar')
     await this.composeWith('./store')
