44d217c18cf24194cc37a40963f13521ee8f1fce
Alexander Schwartzberg <aeksco@gmail.com>
2018-10-19 18:41:22 -0400

    Added api_resource & api_environment generators

---
+++ generator/api_resource/index.js
@@ -0,0 +1,50 @@
+const Generator = require('@codotype/generator')
+
+// // // //
+
+module.exports = class ExpressJsResources extends Generator {
+
+  async forEachSchema({ blueprint, configuration, schema }) {
+
+    // Pulls `generate_api_doc` from configuration.options
+    // Used to conditionally generate APIDoc headers
+    // const { generate_api_doc } = configuration.options
+
+    // Defines the schema-specific destination
+    let resourceDest = 'server/api/' + schema.identifier
+
+    // Ensures the presence of the directory
+    await this.ensureDir(resourceDest)
+
+    // server/api/resource/resource.model.js
+    if (schema.identifier === 'user') {
+      await this.copyTemplate(
+        this.templatePath('user.resource.model.js'),
+        this.destinationPath(resourceDest + '/' + schema.identifier + '.model.js'),
+        { schema }
+      );
+    } else {
+      await this.copyTemplate(
+        this.templatePath('resource.model.js'),
+        this.destinationPath(resourceDest + '/' + schema.identifier + '.model.js'),
+        { schema }
+      );
+    }
+
+    // server/api/resource/resource.controller.js
+    await this.copyTemplate(
+      this.templatePath('resource.controller.js'),
+      this.destinationPath(resourceDest + '/' + schema.identifier + '.controller.js'),
+      { schema, generate_api_doc: false }
+    );
+
+    // server/api/resource/index.js
+    await this.copyTemplate(
+      this.templatePath('index.js'),
+      this.destinationPath(resourceDest + '/index.js'),
+      { schema }
+    );
+
+  }
+
+};
